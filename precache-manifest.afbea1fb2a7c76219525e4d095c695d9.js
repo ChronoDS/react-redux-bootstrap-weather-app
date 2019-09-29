1806814d952f0eec500319662da3febacabfdc8e
daniel.sh.ds@gmail.com <Ds302596>
2019-09-29 12:42:24 +0300

    Updates

---
+++ precache-manifest.afbea1fb2a7c76219525e4d095c695d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2e422bbec65c18c625a999d4920dad1",
+    "url": "/daniel-shema-24-09-19/index.html"
+  },
+  {
+    "revision": "c12328a71e6f677ea6da",
+    "url": "/daniel-shema-24-09-19/static/css/main.58eb5619.chunk.css"
+  },
+  {
+    "revision": "29edd37bb7d9e8ed6a5f",
+    "url": "/daniel-shema-24-09-19/static/js/2.5f17b9d3.chunk.js"
+  },
+  {
+    "revision": "c12328a71e6f677ea6da",
+    "url": "/daniel-shema-24-09-19/static/js/main.4abfe8e8.chunk.js"
+  },
+  {
+    "revision": "805afc7ab8ed3c11d1fc",
+    "url": "/daniel-shema-24-09-19/static/js/runtime-main.62743bf8.js"
+  },
+  {
+    "revision": "3e10ede515f93498a1ba327b60299c61",
+    "url": "/daniel-shema-24-09-19/static/media/tlv_1.3e10ede5.jpg"
+  }
+]);
