836841fb0c1d7737d22f4a3277ce7b168bd80f45
daniel.sh.ds@gmail.com <Ds302596>
2019-10-06 01:35:49 +0300

    fixed some routing issues and added Root component.

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import {Provider} from "react-redux";
 import {store} from '../src/components/Utils/store'
 import {BrowserRouter as Router, Route} from "react-router-dom";
+import Root from "./Root";
 
 export const handleThemeChange = (isChecked) => {
     if (isChecked) {
@@ -14,14 +15,16 @@ export const handleThemeChange = (isChecked) => {
         document.documentElement.setAttribute('data-theme', 'light');
     }
 };
+//
+// ReactDOM.render(
+//     <Provider store={store}>
+//         <Router>
+//             <Route path="/" component={App}/>
+//         </Router>
+//     </Provider>,
+//     document.getElementById('root'));
 
-ReactDOM.render(
-    <Provider store={store}>
-        <Router>
-            <Route path="/" component={App}/>
-        </Router>
-    </Provider>,
-    document.getElementById('root'));
+ReactDOM.render(<Root store={store} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
